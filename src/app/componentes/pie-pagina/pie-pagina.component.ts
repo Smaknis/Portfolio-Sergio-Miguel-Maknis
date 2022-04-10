d992f6a7c5439fd6a233d27951a4b59b2d662afc
Maknis <sergiomaknis@hotmail.com>
2022-04-09 21:06:00 -0300

    Crea Servicios
    Crea servicios en Angular, agrega archivo json con datos de la pagina, aplica databinding e interpolacion.

--- src/app/componentes/pie-pagina/pie-pagina.component.ts
+++ src/app/componentes/pie-pagina/pie-pagina.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-pie-pagina',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pie-pagina.component.css']
 })
 export class PiePaginaComponent implements OnInit {
-
-  constructor() { }
+  footer:any=0;
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.footer=data.proyects;
+    });
   }
 
 }
