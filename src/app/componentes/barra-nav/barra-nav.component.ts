cbca769e5a2f3bebe8b5c2a5181942c6b2356dde
Maknis <sergiomaknis@hotmail.com>
2022-10-27 19:41:52 -0300

    Agrega componente login
    Se agrega componente login, se crea email y constraeña en el front end para validad acceso (no lo hago en el back end porque no se dio en el curso y no llegue a investigar como se hace)

--- src/app/componentes/barra-nav/barra-nav.component.ts
+++ src/app/componentes/barra-nav/barra-nav.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { PortfolioService } from 'src/app/servicios/portfolio.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-barra-nav',
@@ -8,35 +6,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./barra-nav.component.css']
 })
 export class BarraNavComponent implements OnInit {
-  miPortfolio:any=0;
-  form:FormGroup;
-  constructor(private datosPortfolio:PortfolioService, private formBuilder:FormBuilder) {
-    this.form=this.formBuilder.group(
-      {
-        email:['',[Validators.required,Validators.email]],
-        password:['',[Validators.required, Validators.minLength(8)]],
-        deviceInfo:this.formBuilder.group({
-          deviceId: ["17867868768"],
-          deviceType: ["DEVICE_TYPE_ANDROID"],
-          notificationToken:["67657575ececc34"]
-        })
-      }
-    )
+  
+  constructor() {
    }
 
-  ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos().subscribe(data =>{
-      console.log(data);
-      this.miPortfolio=data;
-    });
-  }
-
-  get Email(){
-    return this.form.get('email');
-  }
-
-  get Password(){
-    return this.form.get('password');
+  ngOnInit() {
   }
 
 }
